1b424a9e9125158731347df85b30226f73e816b5
Marek <markuspoland@gmail.com>
2019-11-17 20:27:36 +0100

    swipe ???

---
+++ Assets/Scripts/SwipeManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeManager : MonoBehaviour
+{
+    public static Swipe swipeDirection;
+
+    public float minSwipeLength = 200f;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+    public enum Swipe { None, Up, Down, Left, Right };
+
+
+
+    void update()
+    {
+
+        DetectSwipe();
+    }
+
+
+    public void DetectSwipe()
+    {
+
+
+
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                // Make sure it was a legit swipe, not a tap
+                if (currentSwipe.magnitude < minSwipeLength)
+                {
+                    swipeDirection = Swipe.None;
+                    return;
+                }
+
+                currentSwipe.Normalize();
+
+                if (!(secondPressPos == firstPressPos))
+                {
+                    if (Mathf.Abs(currentSwipe.x) > Mathf.Abs(currentSwipe.y))
+                    {
+                        if (currentSwipe.x < 0)
+                        {
+                            Debug.Log("right");
+                        }
+                        else
+                        {
+                            Debug.Log("left");
+                        }
+                    }
+                    else
+                    {
+                        if (currentSwipe.y < 0)
+                        {
+                            Debug.Log("down");
+                        }
+                        else
+                        {
+                            Debug.Log("up");
+                        }
+                    }
+                }
+
+
+
+            }
+            else
+            {
+                swipeDirection = Swipe.None;
+            }
+        }
+
+    }
+}
--- Assets/SwipeHandler.cs
+++ Assets/SwipeHandler.cs
@@ -4,20 +4,70 @@ using UnityEngine;
 
 public class SwipeHandler : MonoBehaviour
 {
+
+    public AudioClip success;
+    public AudioClip failure;
+    public GameObject successImage;
+    public GameObject failureImage;
+    public Animator successAnim;
+    public Animator failureAnim;
+    public AudioSource audioSource;
     // Start is called before the first frame update
+     void Awake()
+    {
+     
+    }
+
     void OnEnable()
     {
         Invoke("DisableMe", 1.5f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.CompareTag("Left"))
+        {
+            print("I am " + gameObject.tag);
+            if (SwipeManager.swipeDirection == SwipeManager.Swipe.Left)
+            {
+                audioSource.PlayOneShot(success);
+                successAnim.SetTrigger("success");
+
+            } else if (SwipeManager.swipeDirection == SwipeManager.Swipe.Right)
+            {
+                audioSource.PlayOneShot(failure);
+                failureAnim.SetTrigger("fail");
+            }
+        }
+
+        if (gameObject.CompareTag("Right"))
+        {
+            print("I am " + gameObject.tag);
+            if (SwipeManager.swipeDirection == SwipeManager.Swipe.Right)
+            {
+                successAnim.SetTrigger("success");
+                audioSource.PlayOneShot(success);
+            }
+            else if (SwipeManager.swipeDirection == SwipeManager.Swipe.Left)
+            {
+                audioSource.PlayOneShot(failure);
+                failureAnim.SetTrigger("fail");
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        audioSource.PlayOneShot(failure);
         
+        print("FAILURE!!!!!");
     }
 
     void DisableMe()
     {
+        
         gameObject.SetActive(false);
     }
 }
