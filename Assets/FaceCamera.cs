9b44de633abbdb582daa3ba00c2bbb5a33b04394
Marek <markuspoland@gmail.com>
2020-04-29 11:33:16 +0200

    Rage arrow test

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookPos = transform.position - Camera.main.transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = rotation;
+    }
+}
